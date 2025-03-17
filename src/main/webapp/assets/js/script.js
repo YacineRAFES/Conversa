2925daebe4d49f3f7ea3944f37ac1bb090bedc41
Yacine RAFES <134509065+YacineRAFES@users.noreply.github.com>
2025-03-17 16:27:17 +0100

    Modification de l'interface des messages privées

--- src/main/webapp/assets/js/script.js
+++ src/main/webapp/assets/js/script.js
@@ -1,1 +1,11 @@
-ad
+document.addEventListener("click", function (event) {
+    const dropdown = document.querySelector(".dropdown-childCustom");
+    const button = document.querySelector(".mainmenubtn");
+
+    // Vérifie si le clic est sur le bouton ou dans le menu
+    if (button.contains(event.target)) {
+        dropdown.classList.toggle("show"); // Ouvre ou ferme le menu
+    } else if (!dropdown.contains(event.target)) {
+        dropdown.classList.remove("show"); // Ferme si on clique ailleurs
+    }
+});
