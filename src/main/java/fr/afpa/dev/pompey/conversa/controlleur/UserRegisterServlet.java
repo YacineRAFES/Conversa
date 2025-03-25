dcd24d198e5d0d525cc8227528b9aba12a2dd67a
Yacine RAFES <134509065+YacineRAFES@users.noreply.github.com>
2025-03-25 08:51:26 +0100

    Merge front-end + back-end

---
+++ src/main/java/fr/afpa/dev/pompey/conversa/controlleur/UserRegisterServlet.java
@@ -0,0 +1,33 @@
+package fr.afpa.dev.pompey.conversa.controlleur;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "UserRegisterServlet", value = "/UserRegister")
+public class UserRegisterServlet extends HttpServlet {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
