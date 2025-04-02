e8dd903bfdc00b17b0f867c02a331375b987df44
Yacine RAFES <134509065+YacineRAFES@users.noreply.github.com>
2025-04-02 16:37:59 +0200

    Modifcation 02042025

---
+++ src/main/java/fr/afpa/dev/pompey/conversa/servlet/HomeServlet.java
@@ -0,0 +1,36 @@
+package fr.afpa.dev.pompey.conversa.servlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "HomeServlet", value = "/home")
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Définir le titre de la page
+        request.setAttribute("title", "Home");
+        // Définir le nom du fichier JavaScript à inclure
+        request.setAttribute("js", "home.js");
+
+        this.getServletContext().getRequestDispatcher("/JSP/page/home.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+    
+    @Override
+    public void destroy() {
+    
+    }
+}
---
+++ src/main/webapp/assets/js/alertMessages.js
@@ -0,0 +1,29 @@
+// Messages des alertes
+export class AlertMessages{
+    // ErrorServer = "Erreur lors de la réception de la réponse du serveur."
+    static ErrorServer = "Erreur lors de la réception de la réponse du serveur.";
+
+    // UserAlreadyExists = "Le nom ou l'email existe déjà."
+    static UserAlreadyExists = "Le nom ou l'email existe déjà.";
+
+    // passwordInvalid = "Le mdp doit contenir ...";
+    static PasswordInvalid = "Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule et un chiffre.";
+
+    // EmailInvalid = "L'adresse e-mail est invalide."
+    static EmailInvalid = "L'adresse e-mail est invalide.";
+
+    // EmptyField = "Tous les champs doivent être remplis."
+    static EmptyField = "Tous les champs doivent être remplis.";
+
+    // LengthInvalid = "Le nom d'utilisateur ou L'adresse e-mail ne doit pas contenir plus de 50 caractères."
+    static LengthInvalid = "Le nom d'utilisateur ou L'adresse e-mail ne doit pas contenir plus de 50 caractères.";
+
+    // ErrorUserCreated = "Erreur lors de la création du compte."
+    static ErrorUserCreated = "Erreur lors de la création du compte.";
+
+    // InvalidCredentials = "Nom d'utilisateur ou mot de passe incorrect."
+    static InvalidCredentials= "Nom d'utilisateur ou mot de passe incorrect.";
+
+    // ErrorCredentials = "Erreur lors de la connexion du compte."
+    static ErrorCredentials = "Erreur lors de la connexion du compte.";
+}
