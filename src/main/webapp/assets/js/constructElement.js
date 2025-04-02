0e8396d72bbd1bb847732228c9c9547a71c9bd0e
Yacine RAFES <134509065+YacineRAFES@users.noreply.github.com>
2025-04-02 16:22:33 +0200

    Ajoute une nouvelle element pour créer les alertes

--- src/main/webapp/assets/js/constructElement.js
+++ src/main/webapp/assets/js/constructElement.js
@@ -6,3 +6,11 @@ export function csrfInput(token){
     csrfInput.value = token;
     return csrfInput;
 }
+
+export function createAlert(message, type){
+    const alert = document.createElement("div");
+    alert.className = `alert alert-${type} text-center`;
+    alert.role = "alert";
+    alert.innerHTML = message;
+    return alert;
+}
