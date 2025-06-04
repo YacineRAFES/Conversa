8621f0482e84d56d44639ce6c9452e606e1e2112
Yacine RAFES <134509065+YacineRAFES@users.noreply.github.com>
2025-06-04 11:37:04 +0200

    modif

--- src/main/java/fr/afpa/dev/pompey/conversa/servlet/RegisterServlet.java
+++ src/main/java/fr/afpa/dev/pompey/conversa/servlet/RegisterServlet.java
@@ -36,10 +36,10 @@ public class RegisterServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try{
+            String captcha = request.getParameter("cf-turnstile-response");
 
-        String captcha = request.getParameter("cf-turnstile-response");
-
-        log.info("Captcha : " + captcha);
+            log.info("Captcha : " + captcha);
 
 //        boolean isCaptchaValid = Captcha.verif(captcha);
 //        if (!isCaptchaValid) {
@@ -48,58 +48,63 @@ public class RegisterServlet extends HttpServlet {
 //            return;
 //        }
 
-        // 29/04/2025 j'ai mis Map<String, String> formData = new HashMap<>(); en Map<String, Object> formData = new HashMap<>();
-        Map<String, Object> formData = new HashMap<>();
-        formData.put("username", request.getParameter("user"));
-        formData.put("email", request.getParameter("email"));
-        formData.put("password1", request.getParameter("password1"));
-        formData.put("password2", request.getParameter("password2"));
-        log.info("formData : " + formData);
+            // 29/04/2025 j'ai mis Map<String, String> formData = new HashMap<>(); en Map<String, Object> formData = new HashMap<>();
+            Map<String, Object> formData = new HashMap<>();
+            formData.put("username", request.getParameter("user"));
+            formData.put("email", request.getParameter("email"));
+            formData.put("password1", request.getParameter("password1"));
+            formData.put("password2", request.getParameter("password2"));
+            log.info("formData : " + formData);
 
-        Map<String, Object> apiResponse = envoyerFormulaireVersApi(formData, REGISTER);
+            Map<String, Object> apiResponse = envoyerFormulaireVersApi(formData, REGISTER);
 
-        if(apiResponse != null){
-            JsonObject jsonObject = (JsonObject) apiResponse.get("json");
+            if(apiResponse != null){
+                JsonObject jsonObject = (JsonObject) apiResponse.get("json");
 
-            if (jsonObject.getString("status").equals("success")) {
-                log.info("Inscription réussie et redirection vers la page de connexion");
-                sendRedirectTo(request, response, Utils.ServletPage.LOGIN);
+                if (jsonObject.getString("status").equals("success")) {
+                    log.info("Inscription réussie et redirection vers la page de connexion");
+                    sendRedirectTo(request, response, Utils.ServletPage.LOGIN);
 
-            } else if (jsonObject.getString("status").equals("error")) {
-                request.setAttribute("title", "Inscription");
+                } else if (jsonObject.getString("status").equals("error")) {
+                    request.setAttribute("title", "Inscription");
 
-                if (jsonObject.getString(MESSAGE).equals("lengthInvalid")) { // Au cas d'erreur de longueur
-                    log.info("lengthInvalid");
-                    request.setAttribute(SET_DIV_ERROR, Alert.LENGTHINVALID); // Afficher le message d'erreur
-                    GoToPage(request, response, Utils.ServletPage.REGISTER);
+                    if (jsonObject.getString(MESSAGE).equals("lengthInvalid")) { // Au cas d'erreur de longueur
+                        log.info("lengthInvalid");
+                        request.setAttribute(SET_DIV_ERROR, Alert.LENGTHINVALID); // Afficher le message d'erreur
+                        GoToPage(request, response, Utils.ServletPage.REGISTER);
 
-                } else if (jsonObject.getString(MESSAGE).equals("userAlreadyExists")) {
-                    log.info("userAlreadyExists");
-                    request.setAttribute(SET_DIV_ERROR, Alert.USERALREADYEXISTS);
-                    GoToPage(request, response, Utils.ServletPage.REGISTER);
+                    } else if (jsonObject.getString(MESSAGE).equals("userAlreadyExists")) {
+                        log.info("userAlreadyExists");
+                        request.setAttribute(SET_DIV_ERROR, Alert.USERALREADYEXISTS);
+                        GoToPage(request, response, Utils.ServletPage.REGISTER);
 
-                } else if (jsonObject.getString(MESSAGE).equals("emailInvalid")) {
-                    log.info("emailInvalid");
-                    request.setAttribute(SET_DIV_ERROR, Alert.EMAILINVALID);
-                    GoToPage(request, response, Utils.ServletPage.REGISTER);
+                    } else if (jsonObject.getString(MESSAGE).equals("emailInvalid")) {
+                        log.info("emailInvalid");
+                        request.setAttribute(SET_DIV_ERROR, Alert.EMAILINVALID);
+                        GoToPage(request, response, Utils.ServletPage.REGISTER);
 
-                } else if (jsonObject.getString(MESSAGE).equals("passwordInvalid")) {
-                    log.info("passwordInvalid");
-                    request.setAttribute(SET_DIV_ERROR, Alert.PASSWORDINVALID);
-                    GoToPage(request, response, Utils.ServletPage.REGISTER);
+                    } else if (jsonObject.getString(MESSAGE).equals("passwordInvalid")) {
+                        log.info("passwordInvalid");
+                        request.setAttribute(SET_DIV_ERROR, Alert.PASSWORDINVALID);
+                        GoToPage(request, response, Utils.ServletPage.REGISTER);
 
-                } else if (jsonObject.getString(MESSAGE).equals("emptyField")) {
-                    log.info("emptyField");
-                    request.setAttribute(SET_DIV_ERROR, Alert.EMPTYFIELD);
-                    GoToPage(request, response, Utils.ServletPage.REGISTER);
+                    } else if (jsonObject.getString(MESSAGE).equals("emptyField")) {
+                        log.info("emptyField");
+                        request.setAttribute(SET_DIV_ERROR, Alert.EMPTYFIELD);
+                        GoToPage(request, response, Utils.ServletPage.REGISTER);
 
-                } else {
-                    log.info("errorUnknown");
-                    request.setAttribute(SET_DIV_ERROR, Alert.UNKNOWNERROR);
-                    GoToPage(request, response, Utils.ServletPage.REGISTER);
+                    } else {
+                        log.info("errorUnknown");
+                        request.setAttribute(SET_DIV_ERROR, Alert.UNKNOWNERROR);
+                        GoToPage(request, response, Utils.ServletPage.REGISTER);
 
+                    }
                 }
             }
+        }catch (Exception e){
+            log.error("Erreur lors de l'inscription : ", e);
+            request.setAttribute(SET_DIV_ERROR, Alert.ERRORSERVER);
+            GoToPage(request, response, Utils.ServletPage.REGISTER);
         }
     }
 }
